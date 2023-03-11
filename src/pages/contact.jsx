1c36e44fd067af5e15477d85af09ddda5ea683a6
SP-ach <acharyasthitaprangya@gmail.com>
2023-03-11 08:03:54 +0530

    carty

---
+++ src/pages/contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Contact = () => {
+  return <div>
+    
+    Connect with me:
+    <h1><a href="LinkedIn: https://www.linkedin.com/in/sp-acharya/">LinkedIn</a></h1>
+  </div>;
+};
---
+++ src/products.js
@@ -0,0 +1,52 @@
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Three Thousand Stitches",
+    price: 234,
+    productImage: 'https://m.media-amazon.com/images/I/71ygf-LciIS.jpg',
+  },
+  {
+    id: 2,
+    productName: "Rich Dad Poor Dad",
+    price: 339,
+    productImage: 'https://m.media-amazon.com/images/I/81BE7eeKzAL.jpg',
+  },
+  {
+    id: 3,
+    productName: "Godan",
+    price: 299,
+    productImage: 'https://m.media-amazon.com/images/I/71tnQ2XA2uL.jpg',
+  },
+  {
+    id: 4,
+    productName: "You Only Live Once",
+    price: 228,
+    productImage: 'https://m.media-amazon.com/images/I/71dNsRuYL7L.jpg',
+  },
+  {
+    id: 5,
+    productName: "Harry Potter and the Philosophers Stone",
+    price: 199,
+    productImage: 'https://m.media-amazon.com/images/I/5165He67NEL.jpg',
+  },
+  {
+    id: 6,
+    productName: "Chanakya Neeti with Chanakya Sutra Sahit",
+    price: 168,
+    productImage: 'https://m.media-amazon.com/images/I/819VSyPBx0L.jpg',
+  },
+  {
+    id: 7,
+    productName: "Twelfth Fail: Hara Vahi Jo Lada Nahi",
+    price: 120,
+    productImage: 'https://m.media-amazon.com/images/I/81QOMjpuQpL.jpg',
+  },
+  {
+    id: 8,
+    productName: "Think and Grow RIch",
+    price: 240,
+    productImage: 'https://m.media-amazon.com/images/I/71UypkUjStL.jpg',
+  },
+];
